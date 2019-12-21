99caf46e33401dcb6de0eee8fede9f333caa26ca
queueball <queueball314159@gmail.com>
2019-12-21 13:26:51 -0800

    Updated: removing jedi vim and vim go

--- install.sh
+++ install.sh
@@ -3,14 +3,12 @@ echo "Creating pathogen directories"
 mkdir -p ~/home/.vim/autoload ~/home/.vim/bundle && curl -LSso ~/home/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 echo "Cloning useful plugins"
-git clone --recursive https://github.com/davidhalter/jedi-vim.git ~/home/.vim/bundle/jedi-vim
 git clone git://github.com/tpope/vim-repeat.git ~/home/.vim/bundle/vim-repeat
 git clone git://github.com/tpope/vim-surround.git ~/home/.vim/bundle/vim-surround
 git clone https://github.com/vim-airline/vim-airline ~/home/.vim/bundle/vim-airline
 git clone https://github.com/vim-airline/vim-airline-themes ~/home/.vim/bundle/vim-airline-themes
 git clone https://github.com/nvie/vim-flake8 ~/home/.vim/bundle/vim-flake8
 git clone git://github.com/tpope/vim-vinegar.git ~/home/.vim/bundle/vim-vinegar
-git clone https://github.com/fatih/vim-go.git ~/home/.vim/bundle/vim-go
 
 echo "Cloning & installing useful fonts"
 git clone https://github.com/powerline/fonts.git ~/home/.vim/fonts/
