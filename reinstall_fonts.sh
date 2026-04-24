03060a26c8d6cd6c124c98e0ca96915f359009ba
queueball <queueball314159@gmail.com>
2026-04-23 22:37:21 -0700

    Updated: qol improvements

--- reinstall_fonts.sh
+++ reinstall_fonts.sh
@@ -6,4 +6,4 @@ mkdir -p ~/home/.fonts/
 # NOTE this is pretty large (~5 GB at 2022-10)
 git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git ~/home/.fonts/
 ~/home/.fonts/install.sh Hack
-rm -r ~/home/.fonts/
+rm -rf ~/home/.fonts/
